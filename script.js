b03a8e082bf2544891483d81c1593cd49f9711a8
777abat777 <87979902+777abat777@users.noreply.github.com>
2023-07-10 17:12:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+document.querySelector("button").onclick = function () {
+   printRules('findPair/index.html')
+}
+
+function printRules(linkToYourGame) {
+   let rules
+   let arr = []
+   let counter = 0
+   let input = document.querySelector('input')
+   let button = document.querySelector('button')
+   let name = ' '
+
+   name = input.value
+   rules = `Привет, ${name}. Тут будут правила игры`
+   interval = setInterval(iteration, 100)
+   input.remove()
+   button.remove()
+   let createA = document.createElement('a')
+   createA.innerHTML = 'START GAME'
+   createA.href = linkToYourGame
+   document.querySelector('div').append(createA)
+
+
+   function iteration() {
+      arr.push(rules[counter])
+      document.querySelector('p').innerHTML = arr.join('')
+      counter++
+      if (counter > rules.length) {
+         clearInterval(interval)
+
+      }
+   }
+}
+let h1 = document.querySelector('h1').addEventListener("", () => {
+   alert('hello')
+})
